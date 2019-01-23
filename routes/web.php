6c538ae67c33fea8fce44fe6e9dbf8f69560e3e5
maclaude <claude.marcantoine2@gmail.com>
2019-01-23 14:36:15 +0100

    Routes definition

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,46 @@
 |
 */
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
-});
+// $router->get('/', function () use ($router) {
+//     return $router->app->version();
+// });
+
+$router->get('/', [
+    'as' => 'home',
+    'uses' => 'MainController@home',
+]);
+
+$router->get('/quiz/{id}', [
+    'as' => 'quiz',
+    'uses' => 'QuizController@quiz'
+]);
+
+$router->post('/quiz/{id}', [
+    'as' => 'quiz',
+    'uses' => 'QuizController@quiz'
+]);
+
+$router->get('/signup', [
+    'as' => 'signup',
+    'uses' => 'UserController@signup',
+]);
+ 
+$router->post('/signup', [
+    'as' => 'signup',
+    'uses' => 'UserController@signup',
+]);
+
+$router->get('/signin', [
+    'as' => 'signin',
+    'uses' => 'UserController@signin',
+]);
+ 
+$router->post('/signin', [
+    'as' => 'signin',
+    'uses' => 'UserController@signin',
+]);
+
+$router->get('/logout', [
+    'as' => 'logout',
+    'uses' => 'UserController@logout',
+]);
