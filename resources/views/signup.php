a892251ebceaeb808809596e605a6305db7ca22d
maclaude <claude.marcantoine2@gmail.com>
2019-01-24 13:29:36 +0100

    Signup view bootstrap integration

--- resources/views/signup.php
+++ resources/views/signup.php
@@ -1,47 +1,38 @@
 <?= view('layout/header') ?>
 <?= view('layout/nav') ?>
 
-<div class="row">
-    <h2>Se connecter</h2>
-    <div id="login">
-
-        <form action="" id="login-form" method="post" autocomplete="off">
-            <div class="field">
-                <label class="field-label" for="field-firstname">
-                    Prénom
-                </label>
-                <input class="field-input" id="field-firstname" name="firstname" type="text" placeholder="Prénom"
-                    data-kwimpalastatus="alive" data-kwimpalaid="1548248032175-2">
-                <p class="field-info">Obligatoire - doit contenir au minimum 3 caractères</p>
-            </div>
-            <div class="field">
-                <label class="field-label" for="field-lastname">
-                    Nom
-                </label>
-                <input class="field-input" id="field-lastname" name="lastname" type="text" placeholder="Nom"
-                    data-kwimpalastatus="alive" data-kwimpalaid="1548248032175-2">
-                <p class="field-info">Obligatoire - doit contenir au minimum 3 caractères</p>
-            </div>
-            <div class="field">
-                <label class="field-label" for="field-usermail">
-                    Adresse mail
-                </label>
-                <input class="field-input" id="field-usermail" name="usermail" type="text" placeholder="Adresse mail"
-                    data-kwimpalastatus="alive" data-kwimpalaid="1548248032175-2">
-                <p class="field-info">Obligatoire</p>
-            </div>
-            <div class="field">
-                <label class="field-label" for="field-password">
-                    Mot de passe
-                </label>
-                <input class="field-input" id="field-password" name="password" type="password" placeholder="Mot de passe"
-                    data-kwimpalastatus="alive" data-kwimpalaid="1548248032175-3">
-                <p class="field-info">Obligatoire - doit contenir au minimum 3 caractères</p>
-            </div>
-            <div id="errors"></div>
-            <button id="login-submit">Connexion</button>
-        </form>
-    </div>
+<div>
+    <h2>Créer votre compte</h2>
+        <div id="login">
+            <form action="" id="login-form" method="post" autocomplete="off">
+                <div class="form-group">
+                    <label class="field-label" for="field-firstname">
+                        Prénom
+                    </label>
+                    <input class="form-control" id="field-firstname" name="firstname" type="text" placeholder="Prénom">
+                </div>
+                <div class="form-group">
+                    <label class="field-label" for="field-lastname">
+                        Nom
+                    </label>
+                    <input class="form-control" id="field-lastname" name="lastname" type="text" placeholder="Nom">
+                </div>
+                <div class="form-group">
+                    <label class="field-label" for="field-usermail">
+                        Adresse mail
+                    </label>
+                    <input class="form-control" id="field-usermail" name="usermail" type="text" placeholder="Votre adresse email">
+                </div>
+                <div class="form-group">
+                    <label class="field-label" for="field-password">
+                        Mot de passe
+                    </label>
+                    <input class="form-control" id="field-password" name="password" type="password" placeholder="Mot de passe">
+                </div>
+                <button href="" class="btn btn-primary" type="submit" role="button">Inscription</button>
+                <a href="<?= route('signin')?>" class="btn btn-primary" role="button">J'ai déjà un compte</a>
+            </form>
+        </div>
 </div>
 
 <?= view('layout/footer') ?>
